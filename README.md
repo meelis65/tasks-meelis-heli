d7126610a3bbae1e0218a9c7e7d08985e25e14bc
meelis65 <meelis.heli@vikk.ee>
2021-10-03 19:26:09 +0300

    new

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Hi, I'm Meelis Heli
 September 14
 My school work
 Files created in JavaScript
+Task 1-4  Don't understand how
+Task 5 With the help of group brother
+Task 6-7 Chasing letter after letter
+Task 8 The mind collapse
--- task8.js
+++ task8.js
@@ -1,7 +1,68 @@
-const movieData = require(',/moviedata.json')
+const movieData = require('./moviedata.json')
 
-movieDataa.map(movie =>{
-    movie.Year <0 +movieData
+movieData.map(movie => {
+    movie.Year = +movie.Year
+    movie.imdbRating = +movie.imdbRating
+    movie.imdbVotes = +movie.imdbVotes.replace(',', '')
 })
 
-console.log('movie', movieData)
+//console.log('movie', movieData)
+
+//console.log('Ex 1')
+
+const moviesBetween = movieData.filter(movie => {
+    if (movie.Year >= 1962 && movie.Year <=1972){
+        return movie
+    }
+})
+
+//console.log('moviesBetween', moviesBetween)
+
+const bestRating = moviesBetween.reduce((previousValue, currentValue) => {
+    if (previousValue.imbRating > currentValue.imbRating) {
+        return previousValue
+    } else {
+        return currentValue
+    }
+})
+
+//console.log('bestRating', bestRating)
+
+const mostRated = moviesBetween.reduce((perviousValue, currentValue) =>{
+    if (perviousValue.imbVotes > currentValue.imbVotes){
+        return perviousValue
+    }   else {
+        return currentValue
+    }
+})
+
+//console.log('mostRated', mostRated)
+
+let bestRatingString = 'Best rated movie is '
+bestRatingString += bestRating.Title
+bestRatingString += ', directed by '
+bestRatingString += bestRating.Director
+bestRatingString += 'and was released in '
+bestRatingString += bestRating.Released + '.'
+
+console.log(bestRatingString)
+
+let mostRatedString = 'Most rated movie is '
+mostRatedString += mostRated.Title
+mostRatedString += ', direct by '
+mostRatedString += mostRated.Director
+mostRatedString += ' and was released in '
+mostRatedString += mostRated.Released + '.'
+
+console.log(mostRatedString)
+
+
+console.log('Ex 2')
+
+const includesDrama = movieData.filter(movie => {
+    if (movie.Genre.includes('Drama')) {
+        return movie
+    }
+})
+
+console.log('includesDrama', includesDrama)
