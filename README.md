12f36af2f57d71ac88fdb6377130e74ee758bca8
meelis65 <meelis.heli@vikk.ee>
2021-10-09 19:06:57 +0300

    final task

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Files created in JavaScript
 Task 1-4  Don't understand how
 Task 5 With the help of group brother
 Task 6-7 Chasing letter after letter
-Task 8 The mind collapse
+Task 8 Unbelievable that something finally got to work.
+Must practice to be over and over and over again.
--- task8.js
+++ task8.js
@@ -1,21 +1,19 @@
-const movieData = require('./moviedata.json')
+const movies = require('./moviedata.json')
 
-movieData.map(movie => {
+movies.map(movie => {
     movie.Year = +movie.Year
     movie.imdbRating = +movie.imdbRating
     movie.imdbVotes = +movie.imdbVotes.replace(',', '')
+    movie.Actor = movie.Actors.split(', ')
 })
-
 //console.log('movie', movieData)
 
 //console.log('Ex 1')
-
-const moviesBetween = movieData.filter(movie => {
-    if (movie.Year >= 1962 && movie.Year <=1972){
+const moviesBetween = movies.filter(movie => {
+    if (movie.Year >= 1962 && movie.Year <=1972) {
         return movie
     }
 })
-
 //console.log('moviesBetween', moviesBetween)
 
 const bestRating = moviesBetween.reduce((previousValue, currentValue) => {
@@ -25,18 +23,16 @@ const bestRating = moviesBetween.reduce((previousValue, currentValue) => {
         return currentValue
     }
 })
-
 //console.log('bestRating', bestRating)
 
-const mostRated = moviesBetween.reduce((perviousValue, currentValue) =>{
+const mostRating = moviesBetween.reduce((perviousValue, currentValue) =>{
     if (perviousValue.imbVotes > currentValue.imbVotes){
         return perviousValue
     }   else {
         return currentValue
     }
 })
-
-//console.log('mostRated', mostRated)
+//console.log('mostRating', mostRating)
 
 let bestRatingString = 'Best rated movie is '
 bestRatingString += bestRating.Title
@@ -47,22 +43,96 @@ bestRatingString += bestRating.Released + '.'
 
 console.log(bestRatingString)
 
-let mostRatedString = 'Most rated movie is '
+let mostRatingString = 'Most rated movie is '
+mostRatingString += mostRating.Title
+mostRatingString += ', direct by '
+mostRatingString += mostRating.Director
+mostRatingString += ' and was released in '
+mostRatingString += mostRating.Released + '.'
+
+console.log(mostRatingString)
+
+//console.log('Ex 2')
+const genreMovies = movies.filter((movieGenre) => {
+    if (movieGenre.Genre.includes('Drama')) {
+        return movieGenre
+    }
+})
+// console.log('Drama', genreMovies)
+
+const bestRated = genreMovies.reduce((perviousValue, currentValue) => {
+    if (perviousValue.imdbVotes > currentValue.imdbVotes) {
+        return perviousValue
+    }  else{
+        return currentValue
+    }
+})
+// console.log('mostRating', mostRating)
+
+const mostRated = genreMovies.reduce((perviousValue, currentValue) => {
+    if (perviousValue.imbVotes > currentValue.imdbVotes) {
+        return perviousValue
+    } else{
+        return currentValue
+    }
+})
+// console.log('mostRating*, mostRating)
+
+let bestRatedString = 'Best rated drama is '
+bestRatedString += mostRated.Title
+bestRatedString += ', directed by '
+bestRatedString += bestRated.Director
+bestRatedString += ' and was released in '
+bestRatedString += bestRated.Released + '.'
+
+console.log(bestRatedString)
+
+let mostRatedString = 'Mosted rated drama is '
 mostRatedString += mostRated.Title
-mostRatedString += ', direct by '
-mostRatedString += mostRated.Director
+mostRatedString += ', directed by '
+mostRatedString += mostRating.Director
 mostRatedString += ' and was released in '
 mostRatedString += mostRated.Released + '.'
 
 console.log(mostRatedString)
 
+//console.log('Ex 3*)
 
-console.log('Ex 2')
+function getMoviesR(Rated) {
+    return movies.filter((Ratte) => {
+        return Rated.Rated === Rated
+    })
+}
+const rMovies = getMoviesR('R')
+//console.log('rMovies, rMovies)´
 
-const includesDrama = movieData.filter(movie => {
-    if (movie.Genre.includes('Drama')) {
-        return movie
-    }
+const ratedRTitles = rMovies.map(movie => {
+    return movie.Title
 })
+//console.log('raatedRTitles', ratedRTitles)
 
-console.log('includesDrama', includesDrama)
+let rateString = 'Movies that are rated R are: '
+for (let i = 0; i < ratedRTitles.lenght; i++) {
+    const ending = i + 1 === ratedRTitles.lenght
+    if (ending) {
+        rateString += ratedRTitles[i] + ','
+    }else {
+        rateString += ratedRTitles[i] + ','
+    }
+}
+console.log(rateString)
+
+let actors = []
+rMovies.map(movie => {
+    movieActor.map(actor => {
+        actor.push(actor)
+    })
+})
+//console.log(actors)
+
+let uniqeActors = [...new Set(actors)]
+// console.log('uniqeActors', uniqueActors)
+
+let actorString = 'Actors that palyed in that movie '
+actorString += uniqeActors.join(', ') + '.'
+console.log(actorString)
